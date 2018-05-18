be5eabd812e48955684c51b61335657b192d48b5
span104 <saketj@spanidea.com>
2018-05-18 14:36:53 +0530

    modified queries

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,27 +3,27 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
-var id = new ObjectID('5afe775a733094241bf1c509');
+var id = '5afe775a733094241bf1c509';
 
 if(!ObjectID.isValid(id)){
     console.log('Invalid Id');
 }
 
-Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log('Todos: ', todos);
-});
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos: ', todos);
+// });
 
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log('Todo: ', todo);
-});
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo: ', todo);
+// });
 
-Todo.findById({id}).then((todo) => {
+Todo.findById(id).then((todo) => {
     if(!todo){
         return console.log('Id not found');
     }
-    cosnole.log('Todo by Id: ', todo);
+    console.log('Todo by Id: ', todo);
 }).catch((e) => console.log(e));
