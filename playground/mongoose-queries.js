c8bbaf90e3e6e69575878f4b72898bbf92707003
span104 <saketj@spanidea.com>
2018-05-18 13:27:19 +0530

    Add queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,29 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = new ObjectID('5afe775a733094241bf1c509');
+
+if(!ObjectID.isValid(id)){
+    console.log('Invalid Id');
+}
+
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('Todos: ', todos);
+});
+
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log('Todo: ', todo);
+});
+
+Todo.findById({id}).then((todo) => {
+    if(!todo){
+        return console.log('Id not found');
+    }
+    cosnole.log('Todo by Id: ', todo);
+}).catch((e) => console.log(e));
