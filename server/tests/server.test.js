b38c899a45e0375240a58140b8bad780f590ddbd
span104 <saketj@spanidea.com>
2018-05-21 18:42:12 +0530

    test the DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,3 +101,41 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toBeFalsy();
+                    // expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .delete('/todos/6afe775a733094241bf1c509')
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if Object Id is invalid', (done) => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+});
